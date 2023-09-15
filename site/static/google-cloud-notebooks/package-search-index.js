575be07708a89e1f982a6e9f794ae47fb729e7cc
GitHub Action <action@github.com>
2023-09-15 16:13:11 +0000

    Add Javadocs for google-cloud-java modules

--- site/static/google-cloud-notebooks/package-search-index.js
+++ site/static/google-cloud-notebooks/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.cloud.notebooks.v1"},{"l":"com.google.cloud.notebooks.v1.stub"},{"l":"com.google.cloud.notebooks.v1beta1"},{"l":"com.google.cloud.notebooks.v1beta1.stub"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.cloud.notebooks.v1"},{"l":"com.google.cloud.notebooks.v1.stub"},{"l":"com.google.cloud.notebooks.v1beta1"},{"l":"com.google.cloud.notebooks.v1beta1.stub"},{"l":"com.google.cloud.notebooks.v2"},{"l":"com.google.cloud.notebooks.v2.stub"}]
